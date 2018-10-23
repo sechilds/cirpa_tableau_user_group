d3b8eeaafb99446aad9e4feafa3faa31d245fef7
Stephen Childs <sechilds@gmail.com>
2018-10-23 14:00:58 -0600

    Add bad words list.
    Pre session edits.

--- process_survey.R
+++ process_survey.R
@@ -2,7 +2,9 @@ library(tidyverse)
 library(haven)
 library(labelled)
 
-survey <- read_sav('~/Downloads/CIRPA Tableau User Group_October 23, 2018_05.58.sav')
+source('bad_word_list.R')
+
+survey <- read_sav('~/Downloads/CIRPA Tableau User Group_October 23, 2018_08.50.sav')
 
 survey_questions_only <- survey[18:ncol(survey)]
 
@@ -17,13 +19,16 @@ questions <- questions %>%
 survey_questions_only %>%
   mutate_all(to_character) %>%
   mutate(respondent_id = 1:n()) %>%
-  gather(field_name, label, -respondent_id) -> response_label
+  gather(field_name, label, -respondent_id) %>%
+  mutate(lower_label = tolower(label)) -> response_label
 survey_questions_only %>%
   mutate(respondent_id = 1:n()) %>%
-  gather(field_name, response, -respondent_id) -> response
-response %>%
+  gather(field_name, response, -respondent_id) -> response_original
+response_original %>%
   inner_join(response_label, by = c('respondent_id', 'field_name')) %>%
-  mutate(score = as.numeric(response)) -> responses
+  mutate(score = as.numeric(response)) %>%
+  filter(!is.na(response)) %>%
+  filter(response!='') -> responses
 
 responses %>%
   inner_join(questions, by='field_name') -> survey_out
